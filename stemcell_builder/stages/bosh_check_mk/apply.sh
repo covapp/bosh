c7ab4a33d72711eb66c9f6c36313d776bacbda3d
covapp <simon.gorial@covisint.com>
2015-05-04 16:54:16 +0000

    Adding staging step for Nagios check_mk agent.

---
+++ stemcell_builder/stages/bosh_check_mk/apply.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+
+set -e
+
+base_dir=$(readlink -nf $(dirname $0)/../..)
+source $base_dir/lib/prelude_apply.bash
+source $base_dir/lib/prelude_bosh.bash
+
+cd $assets_dir/check-mk
+
+# Download and install inet using package manager
+pkg_mgr install xinetd
+
+# Install check-mk agent
+run_in_bosh_chroot $chroot "dpkg -i check-mk-agent_1.2.4p5-2_all.deb"
